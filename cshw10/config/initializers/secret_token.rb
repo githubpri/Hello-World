0bb35abe28b251fb209c3a375a864e4a9a972cd6
bikle611 <bikle611@h2.(none)>
2011-08-28 17:19:16 -0400

    cshw10

---
+++ cshw10/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cshw10::Application.config.secret_token = '56d70056cdc40e0c26c6eeca09631cab5ea5d47645e3a4c069b358be0475db755a74c7e5b65e451d12977931ecab9652dacce5ef731e3dcab8de8ded5356727b'
